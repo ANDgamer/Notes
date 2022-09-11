0266352b22c9d8d529967a0f581255bebc34fe1f
ANDgamer <andriychernysh88@gmail.com>
2022-09-12 02:59:55 +0300

    Improved sidebar

---
+++ main.js
@@ -0,0 +1,23 @@
+const Sidebar = document.getElementById("mySidebar");
+const Body = document.querySelectorAll("body");
+
+function SidebarBut() {
+	if (Sidebar.classList.contains("close")) {
+		Sidebar.classList.remove("close");
+		Sidebar.classList.add("open");
+	} 
+	else if (Sidebar.classList.contains("open")) {
+		Sidebar.classList.remove("open");
+		Sidebar.classList.add("close");
+	}
+	else {
+		Sidebar.classList.add("open");
+	}
+}
+
+function sidebarCl() {
+	if (Sidebar.classList.contains("open")) {
+		Sidebar.classList.remove("open");
+		Sidebar.classList.add("close");
+	}
+}
