263d3d54e89098fdb629308ced6783a95e6445aa
ANDgamer <andriychernysh88@gmail.com>
2022-09-12 13:10:08 +0300

    Test with two sidebars

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const Sidebar = document.getElementById("mySidebar");
 const Body = document.querySelectorAll("body");
+const Sidebar = document.getElementById("mySidebar");
+const Sidenebar = document.getElementById("mySidenebar");
 
 function SidebarBut() {
 	if (Sidebar.classList.contains("close")) {
@@ -13,11 +14,37 @@ function SidebarBut() {
 	else {
 		Sidebar.classList.add("open");
 	}
+	if (Sidenebar.classList.contains("open")) {
+		Sidenebar.classList.remove("open");
+		Sidenebar.classList.add("close");
+	}
+}
+
+function SidenebarBut() {
+	if (Sidenebar.classList.contains("close")) {
+		Sidenebar.classList.remove("close");
+		Sidenebar.classList.add("open");
+	} 
+	else if (Sidenebar.classList.contains("open")) {
+		Sidenebar.classList.remove("open");
+		Sidenebar.classList.add("close");
+	}
+	else {
+		Sidenebar.classList.add("open");
+	}
+	if (Sidebar.classList.contains("open")) {
+		Sidebar.classList.remove("open");
+		Sidebar.classList.add("close");
+	}
 }
 
 function SidebarCl() {
 	if (Sidebar.classList.contains("open")) {
 		Sidebar.classList.remove("open");
 		Sidebar.classList.add("close");
+	}
+	if (Sidenebar.classList.contains("open")) {
+		Sidenebar.classList.remove("open");
+		Sidenebar.classList.add("close");
 	}
 }
